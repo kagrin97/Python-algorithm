7be1ed4bd3abb1b5f71d5ef6e92c7f9e07027955
MinGyu-Kang <75124028+kagrin97@users.noreply.github.com>
2021-04-29 18:55:54 +0900

    modify

--- python/연습장.py
+++ python/연습장.py
@@ -1,36 +1,72 @@
-def check(a):
-    for x, y, what in a:
-        if what == 0:
-            if y == 0 or [x-1, y, 1] in a or [x, y, 1] in a or [x, y-1, 0] in a:
-                continue
-            else:
-                return False
-        elif what == 1:
-            if [x, y-1, 0] in a or [x+1, y-1, 0] in a or ([x-1, y, 1] in a and [x+1, y, 1] in a):
-                continue
-            else:
-                return False
-    return True
+from collections import deque
 
-def solution(n, build_frame):
-    answer = []
+def end_game(b):
+    if b.count('0') == 16:
+        return True
+    return False
 
-    for x, y, what, how in build_frame:
-        if how:
-            answer.append([x, y, what])
-            if not check(answer):
-                answer.remove([x, y, what])
+def remove_element(b, e):
+    b = b.replace(e, '0')
+    return b
+
+def move(b, y, x, dy, dx):
+    ny, nx = y + dy, x + dx
+    if 0 <= nx < 4 and 0 <= ny < 4 and b[ny * 4 + nx] == '0':
+        return move(b, ny, nx, dy, dx)
+    else:
+        if 0 <= nx < 4 and 0 <= ny < 4:
+            return (ny, nx)
         else:
-            answer.remove([x, y, what])
-            if not check(answer):
-                answer.append([x, y, what])
+            return (y, x)
 
-    answer.sort()
+def solution(board, r, c):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    b = ""
+    for i in range(4):
+        for j in range(4):
+            b += str(board[i][j])
+    q = deque()
 
+    cnt = 0
+    enter = -1
+    q.append((r, c, b, cnt, enter))
+    s = set()
+
+    while q:
+        y, x, b, c, e = q.popleft()
+        pos = 4 * y + x
+
+        if (y, x, b, e) in s:
+            continue
+        s.add((y, x, b, e))
+        if end_game(b):
+            return c
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < 4 and 0 <= ny < 4:
+                q.append((nx, ny, b, c+1, e))
+        
+        for i in range(4):
+            ny, nx = move(b, y, x, dy[i], dx[i])
+            if ny == y and nx == x:
+                continue
+            q.append((ny, nx, b, c+1, e))
+        
+        if b[pos] != 0:
+            if e == -1:
+                n_e = pos
+                q.append((y, x, b, c+1, n_e))
+            else:
+                if e != pos and b[e] == b[pos]:
+                    b = remove_element(b, b[e])
+                    q.append((y, x, b, c+1, -1))
     return answer
 
-n = 5
-build_frame = [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],
-[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]
 
-print(solution(n, build_frame))
+board = [[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]]
+r = 1
+c = 0
+print(solution(board, r, c))
---
+++ python/외벽 점검.py
@@ -0,0 +1,39 @@
+from itertools import permutations
+def solution(n, weak, dist):
+    weak_length = len(weak)
+    for i in range(weak_length):
+        weak.append(weak[i] + n)
+
+    answer = len(dist) + 1
+    for i in range(weak_length):     
+        start_point = [weak[j] for j in range(i, i + weak_length)]
+        candidates = permutations(dist, len(dist))
+
+        for order in candidates:
+            friend_idx, friend_count = 0, 1
+            possible_check_length = start_point[0] + order[friend_idx]
+            
+            for idx in range(weak_length):
+                if start_point[idx] > possible_check_length:
+                    friend_count += 1
+                    if friend_count > len(order):
+                        break
+                    friend_idx += 1
+                    possible_check_length = order[friend_idx] + start_point[idx]
+            
+            answer = min(answer, friend_count)
+
+    if answer > len(dist):
+        return -1
+    return answer
+
+n = 12
+weak = [1, 5, 6, 10]
+dist = [1, 2, 3, 4]
+
+print(solution(n, weak, dist))
+
+'''
+이문제는 https://inspirit941.tistory.com/185 에서 풀이방법을 참고했다
+아직도 어렵다
+'''
