70b2f3f2d6044370d8dc788b74c87defaa8ec82b
MinGyu-Kang <75124028+kagrin97@users.noreply.github.com>
2022-06-18 18:05:44 +0900

    백준

--- python/간단한 문제/괄호 회전하기.py
+++ python/간단한 문제/괄호 회전하기.py
@@ -1,36 +1,37 @@
-def solution(s):
-    answer = -1
-    x = len(s)
-    cnt = 0
-    for i in range(x):
-      stack = []
-      flag = True
-      if i != 0:
-        s += s[0]
-        s = s[1:]
-      
-      for k in range(len(s)):
-        if s[k] == '(' or s[k] == '[' or s[k] == '{':
-          stack.append(s[k])
-        else:
-          try:
-            if s[k] == ')' and '(' == stack[-1]:
-              stack.pop()
-            elif s[k] == ']' and '[' == stack[-1]:
-              stack.pop()
-            elif s[k] == '}' and '{' == stack[-1]:
-              stack.pop()
-            else:
-              flag = False
-              break
-          except:
-            flag = False
-            break
-      
-      if flag and not stack:
-        cnt += 1
+def check(s):
+  stack = []
+  for i in s:
+    if not stack:
+      stack.append(i)
+    elif stack[-1] == '(' and i == ')':
+      stack.pop()
+    elif stack[-1] == '[' and i == ']':
+      stack.pop()
+    elif stack[-1] == '{' and i == '}':
+      stack.pop()
+    else:
+      stack.append(i)
 
-    return cnt
+  if stack:
+    return False
+  else:
+    return True
+
+
+def solution(s):
+  cnt = 0
+
+  if len(s) == 1:
+    return 0
+
+  if check(s):
+    cnt += 1
+
+  for i in range(len(s) - 1):
+    s = s[1:] + s[0]
+    if check(s):
+      cnt += 1
+  return cnt
 
 s = "([{)}]"
 print(solution(s))
