1519ddb8327deebbaef052581bcc191f54a38b4d
MinGyu-Kang <75124028+kagrin97@users.noreply.github.com>
2022-06-21 19:14:38 +0900

    갱신

--- python/프로그래머스/Level-3/베스트앨범.py
+++ python/프로그래머스/Level-3/베스트앨범.py
@@ -1,37 +1,37 @@
-from collections import defaultdict
 def solution(genres, plays):
-    answer = []
-    gen_dic = defaultdict(int) # defaultdict를 사용하면 if문을써서
-    idx_dic = defaultdict(list) # 키값이 있는지 없는지 확인하지 않아도됨
+    ans = []
+    play_cnt = []
+    music = {}
+    for i in range(len(plays)):
+        if genres[i] in music:
+            music[genres[i]] += [[i, plays[i]]]  # 고유번호 i와 플레이 횟수를 넣어줌
+        else:
+            music[genres[i]] = [[i, plays[i]]]
 
-    for i in range(len(genres)):
-        gen_dic[genres[i]] += plays[i] # 재생횟수 총합 딕셔너리
-        idx_dic[genres[i]].append((plays[i], i)) # 재생횟수와 인덱스 
+    for gen in music:
+        tmp_cnt = 0
+        for info in music[gen]:  # 해당 장르곡들의 횟수들을 더해줌
+            tmp_cnt += info[1]
+        play_cnt.append([gen, tmp_cnt])  # 장르와 총 횟수를 넣어줌
 
-    gen_dic = sorted(gen_dic.items(), key=lambda x: -x[1])
+    play_cnt = sorted(play_cnt, key=lambda x: x[1], reverse=True)  # 장르별 플레이횟수 정렬
 
-    for i in idx_dic:
-        idx_dic[i] = sorted(idx_dic[i], key=lambda x:-x[0])[:2]
-        # 이문제는 최대 2개를 뽑기 때문에 2개만을 잘라준다
-    while len(gen_dic) > 0:
-        gen_max = gen_dic.pop(0)
-        for i in idx_dic: 
-            if i == gen_max[0]: # 두개의 dict의 장르가 일치하면
-                if len(idx_dic[i]) > 1: # 값이 2개 존재하면
-                    answer.append(idx_dic[i][0][1])
-                    answer.append(idx_dic[i][1][1])
-                    # 2개씩 한번에 넣어줌
-                else:
-                    answer.append(idx_dic[i][0][1])
-                    # 하나만 있으면 하나만 넣음
-    return answer
+    for g, _ in play_cnt:
+        tmp_g = sorted(music[g], key=lambda x: int(x[1]), reverse=True)  # 해당장르 내에서 많이 재생된노래 정렬
+
+        if len(tmp_g) == 1:  # 해당장르노래가 1개만있을경우 하나만 넣음
+            ans.append(tmp_g[0][0])
+        else:
+            for i in range(2):  # 2곡이상 있으면 2곡만 넣어줌
+                ans.append(tmp_g[i][0])
+
+    return ans
 
 genres = ["classic", "pop", "classic", "classic", "pop"]
 plays = [500, 600, 150, 800, 2500]
 print(solution(genres, plays))
 
 '''
-이문제는 함정이 있는데 무조건 장르마다 최대 2개씩만 뽑아야한다
-어이없는것은 문제 지문에 이 내용이 없고 output값에서 발견해야됨 ;;
-이문제 14번째줄처럼 미리 잘라주면 속도가 더 빨라짐
+2022 06 21에 다시풀음 
+이번에는 혼자서 풀었당
 '''
--- python/행렬 테두리 회전하기.py
+++ python/행렬 테두리 회전하기.py
@@ -1,41 +1,39 @@
 def solution(rows, columns, queries):
     answer = []
-    board = [[] for _ in range(rows)]
-    
-    # 1 ~ row*col까지 숫자가 들어간 보드 만들기
-    for i in range(1, rows+1):
-        for j in range(1, columns+1):
-            board[i-1].append((i-1)*columns+j)
+    matrix = [[0 for i in range(columns + 1)] for j in range(rows + 1)]  # 0,0,0,보드판생성(계산용이하게 1씩더)
+    num = 1
+    for row in range(1, rows + 1):
+        for column in range(1, columns + 1):
+            matrix[row][column] = num  # (1,1)부터 (r+1,c+1)까지 1 ~ r*c까지 추가
+            num += 1
 
     for x1, y1, x2, y2 in queries:
-        temp = board[x1-1][y2-1] # 오른쪽위 대각선값 저장
-        min_value = 10001
-        
-        # 북쪽 테두리
-        min_value = min(min(board[x1-1][y1-1:y2-1]), min_value)
-        board[x1-1][y1:y2] = board[x1-1][y1-1:y2-1]
+        tmp = matrix[x1][y1]  # 하나 빼놓는다
+        mini = tmp  # 최초 최소값을 설정
 
-        # 서쪽 테두리
-        for i in range(x1, x2):
-            min_value = min(board[i][y1-1], min_value)
-            board[i-1][y1-1] = board[i][y1-1]
-        
-        # 남쪽 테두리
-        min_value = min(min(board[x2-1][y1:y2]), min_value)
-        board[x2-1][y1-1:y2-1] = board[x2-1][y1:y2]
+        for k in range(x1, x2):  # 좌측
+            test = matrix[k + 1][y1]  # 옮길 숫자
+            matrix[k][y1] = test  # 바뀐 숫자
+            mini = min(mini, test)  # 최소값을 찾는다
 
-        # 동쪽 테두리
-        for i in range(x2-2, x1-2, -1):
-            min_value = min(board[i][y2-1], min_value)
-            board[i+1][y2-1] = board[i][y2-1]
-        
-        board[x1][y2-1] = temp 
-        min_value = min(min_value, temp)
-        # 북쪽 테두리 변화로 변화된값에 의해 오른쪽위 대각선 바로 아래 값이
-        # 이상하게 변하기 때문에 따로 저장해서 넣어줌
-        
-        answer.append(min_value)
-    
+        for k in range(y1, y2):  # 하단
+            test = matrix[x2][k + 1]
+            matrix[x2][k] = test
+            mini = min(mini, test)
+
+        for k in range(x2, x1, -1):  # 우측
+            test = matrix[k - 1][y2]
+            matrix[k][y2] = test
+            mini = min(mini, test)
+
+        for k in range(y2, y1, -1):  # 상단
+            test = matrix[x1][k - 1]
+            matrix[x1][k] = test
+            mini = min(mini, test)
+
+        matrix[x1][y1 + 1] = tmp  # 남은 숫자 push
+        answer.append(mini)  # 작은값을 넣어줌
+
     return answer
 rows = 6
 columns = 6
